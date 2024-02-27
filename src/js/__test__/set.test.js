8a9ac7ab24ef1862aaa71eda9f9fd5a08d7f13ee
Anton Demidov <anton_demidov@list.ru>
2024-02-28 00:50:07 +0300

    modified:   src/js/__test__/set.test.js

--- src/js/__test__/set.test.js
+++ src/js/__test__/set.test.js
@@ -1,57 +1,55 @@
-import { Team, Character } from "../set";
+import { Team, Character } from '../set';
 
-
-test("Проверка создания объекта на основе класса Character", () => {
-    const person = new Character('Max');
-    expect(person).toEqual({player: 'Max'});
+test('Проверка создания объекта на основе класса Character', () => {
+  const person = new Character('Max');
+  expect(person).toEqual({ player: 'Max' });
 });
 
 test('Проверка добавления игрока в команду - метод add()', () => {
-    expect(() => {
-        const team = new Team();
-        team.add('Max');
-    }).not.toThrow();
+  expect(() => {
+    const team = new Team();
+    team.add('Max');
+  }).not.toThrow();
 });
 
 test('Проверка добавления игрока в команду - метод add() #2', () => {
-    const team = new Team();
-    team.add('Max');
-    expect(team.members).toEqual(new Set(['Max']));
+  const team = new Team();
+  team.add('Max');
+  expect(team.members).toEqual(new Set(['Max']));
 });
 
 test('Проверка выброса ошибки при попытки дублирования игрока - метод add()', () => {
-    expect(() => {
-        const team = new Team();
-        team.add('Max');
-        team.add('Max');
-    }).toThrow();
+  expect(() => {
+    const team = new Team();
+    team.add('Max');
+    team.add('Max');
+  }).toThrow();
 });
 
 test('Проверка добавления произвольного количества игроков в команду - метод addAll()', () => {
-    const expected = ["Max", "Max1", "Max2", "Max3"]
-    expect(() => {
-        const team = new Team();
-        team.addAll('Max', 'Max1', 'Max2', 'Max3');
-    }).not.toThrow();
+  expect(() => {
+    const team = new Team();
+    team.addAll('Max', 'Max1', 'Max2', 'Max3');
+  }).not.toThrow();
 });
 
 test('Проверка добавления произвольного количества игроков в команду - метод addAll() #2', () => {
-    const expected = new Set (["Max", "Max1", "Max2", "Max3"])
-        const team = new Team();
-        team.addAll('Max', 'Max1', 'Max2', 'Max3');
-    expect(team.members).toEqual(expected);
+  const expected = new Set(['Max', 'Max1', 'Max2', 'Max3']);
+  const team = new Team();
+  team.addAll('Max', 'Max1', 'Max2', 'Max3');
+  expect(team.members).toEqual(expected);
 });
 
 test('Проверка преобразования Set в массив - метод toArray', () => {
-    const team = new Team();
-    expect(() => {
-        team.toArray();
-    }).not.toThrow();
+  const team = new Team();
+  expect(() => {
+    team.toArray();
+  }).not.toThrow();
 });
 
 test('Проверка преобразования Set в массив - метод toArray #2', () => {
-    const expected = ["Max", "Max1", "Max2", "Max3"];
-    const team = new Team();
-    team.addAll('Max', 'Max1', 'Max2', 'Max3');
-    expect(team.toArray()).toEqual(expected);
+  const expected = ['Max', 'Max1', 'Max2', 'Max3'];
+  const team = new Team();
+  team.addAll('Max', 'Max1', 'Max2', 'Max3');
+  expect(team.toArray()).toEqual(expected);
 });
